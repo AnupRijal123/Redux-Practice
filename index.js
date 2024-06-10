ea95fa199fb63d9470dc8bc2f5efb2dd41fb8cc5
Anup <anuprijal321@gmail.com>
2024-06-10 16:41:18 +0545

    made multiple reducer

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const redux = require('redux');
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 /////////////////// ACTION START ////////////////////
 const BUY_CAKE = 'BUY_CAKE';
 //left is constant and right is action name
@@ -17,6 +18,14 @@ function buyCake() {
         info: 'first redux action'
     }
 }
+
+const BUY_ICECREAM = 'BUY_ICECREAM';
+
+function buyIcecream() {
+    return {
+        type: BUY_ICECREAM,
+    }
+}
 //buyCake() function is action creator that returns action
 /////////////////// ACTION END ////////////////////
 
@@ -24,24 +33,66 @@ function buyCake() {
 /////////////////// REDUCER START ////////////////////
 //(previousState,action)=>newState
 
-const initialState = {
-    numberOfCakes: 10,
-};
-//state should be written inside single object
+/*
+ const initialState = {
+     numberOfCakes: 10,
+     numberOfIceCreams: 20
+ };
+ // this is state,state should be written inside single object
+    OR
+ */
 
-const reducer = (state = initialState, action) => {
+const initialCakeState = {
+    numberOfCakes: 10,
+}
+
+const initialIceCreamState = {
+    numberOfIceCreams: 20,
+}
+
+
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case BUY_CAKE: return {
+//             ...state, //state ko copy banako so that if state vitra aru property haru xa vane it doesnot get affected
+//             numberOfCakes: state.numberOfCakes - 1
+//         }
+//         case BUY_ICECREAM: return {
+//             ...state,
+//             numberOfIceCreams: state.numberOfIceCreams - 1
+
+//         }
+//         default: return state;
+//     }
+// }
+//     OR
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch (action.type) {
         case BUY_CAKE: return {
-            ...state, //state ko copy banako so that if state vitra aru property haru xa vane it doesnot get affected
+            ...state,
             numberOfCakes: state.numberOfCakes - 1
         }
         default: return state;
     }
-}
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch (action.type) {
+        case BUY_ICECREAM: return {
+            ...state,
+            numberOfIceCreams: state.numberOfIceCreams - 5,
+        }
+        default: return state;
+    }
+};
 
 /////////////////// REDUCER END ////////////////////
 
-
+const reducer = combineReducers({
+    cake: cakeReducer,
+    icecream: iceCreamReducer
+});
 const store = createStore(reducer);
 console.log('initial state', store.getState());
 
@@ -52,8 +103,10 @@ store.subscribe(() => {
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
-
 // const unsubscribe = store.subscribe(() => {
 //     console.log('updated store', store.getState());
 // })
 // unsubscribe();
+
+store.dispatch(buyIcecream());
+store.dispatch(buyIcecream());
